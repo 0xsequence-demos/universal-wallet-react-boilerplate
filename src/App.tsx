0ca79928a32caafceed6fa1a7f34662e73819e55
moskalyk <morgan.moskalyk@protonmail.ch>
2024-09-20 12:57:49 -0400

    added project

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import { useState, useEffect} from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { sequence } from '0xsequence'
+
+function App() {
+  
+  sequence.initWallet(import.meta.env.VITE_PROJECT_ACCESS_KEY, {defaultNetwork: 'arbitrum-sepolia'})
+  
+  const [isLoggedIn, setIsLoggedIn] = useState<any>(false)
+
+  const signIn = async () => {
+    const wallet = await sequence.getWallet()
+    const details = await wallet.connect({app: 'react starter boilerplate'})
+    if(details.connected){
+      setIsLoggedIn(true)
+    }
+  }
+
+  useEffect(() => {
+
+  },[isLoggedIn])
+
+  return (
+    <>
+      <div>
+      <h1 className='title'>Sequence Universal Wallet Starter - React</h1>
+      <br/>
+      <br/>
+      {isLoggedIn && <div className='sign-out-button' onClick={()=> setIsLoggedIn(false)}>sign out</div>}
+      <div className='container'>
+      {!isLoggedIn ? <button onClick={() => signIn()}>sign in</button> : 'isConnected'}
+      </div>
+      </div>
+
+    </>
+  )
+}
+
+export default App
